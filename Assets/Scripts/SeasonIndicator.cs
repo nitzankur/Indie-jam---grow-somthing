37f067e2b6419f26ced2216d18dc728068d775cf
Tamir Sagi <tamirsagi@cs.huji.ac.il>
2022-03-15 18:38:02 +0200

    Started work on seasons indicator

---
+++ Assets/Scripts/SeasonIndicator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonIndicator : MonoBehaviour
+{
+    [SerializeField] private float minPerYear = 1f;
+    private Rigidbody2D _rb;
+    private float updateTime = 1f;
+    
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        //_rb.AddTorque(360, ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var rot = transform.rotation;
+        //var degPerSec = 360 / 60 * minPerYear;
+        transform.rotation = rot * Quaternion.Euler(0, 0, -minPerYear*Time.deltaTime);
+    }
+}
