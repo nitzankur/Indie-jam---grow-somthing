60fe7b147d445ec8c95ecc8bcbc2303914a78b2f
nitzankur <nitzan.knoll@mail.huji.ac.il>
2022-03-15 19:05:47 +0200

    with world movement

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float fastParamter;
+    private bool _worldMove = true;
+
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (!_worldMove)
+            {
+                fastParamter = 0;
+            }
+            var rot = transform.rotation;
+            transform.rotation = rot * Quaternion.Euler(0, 0, -fastParamter*Time.deltaTime); 
+        }
     }
 }
