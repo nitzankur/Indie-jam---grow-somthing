182f3e94efc77b13d8771026c0d92aac1cbc88d5
Tamir Sagi <tamirsagi@cs.huji.ac.il>
2022-03-15 19:49:48 +0200

    Added timer for seasons

--- Assets/Scripts/SeasonIndicator.cs
+++ Assets/Scripts/SeasonIndicator.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class SeasonIndicator : MonoBehaviour
 {
-    [SerializeField] private float minPerYear = 1f;
-    private Rigidbody2D _rb;
-
-
-    void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-        //_rb.AddTorque(360, ForceMode2D.Impulse);
-
-    }
+    [Range(0.1f, 30)] [SerializeField] private float minPerYear = 15f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        var degPerSec = 6 / minPerYear;
         var rot = transform.rotation;
-        var degPerSec = 360 / 60;
-        transform.rotation = rot * Quaternion.Euler(0, 0, -360 *Time.deltaTime);
+        transform.rotation = rot * Quaternion.Euler(0, 0, -degPerSec * Time.deltaTime);
+        //print(transform.rotation.z);
+        //print(GetSeason());
+    }
+
+    public string GetSeason()
+    {
+        var season = transform.rotation.z;
+        if (season > -90 && season <= 0) return "Spring";
+        if (season > -180 && season <= -90) return "Summer";
+        if (season <= 180 && season > 90) return "Autumn";
+        return "Winter";
     }
 }
