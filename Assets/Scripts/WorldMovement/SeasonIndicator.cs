87901ef552927c3b99706c82e6258cc71381a7a9
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2022-03-26 17:40:36 +0300

    Fixed season indicator

--- Assets/Scripts/WorldMovement/SeasonIndicator.cs
+++ Assets/Scripts/WorldMovement/SeasonIndicator.cs
@@ -11,5 +11,15 @@ public class SeasonIndicator : MonoBehaviour
         var degPerSec = 6 / minPerYear;
         var rot = transform.rotation;
         transform.rotation = rot * Quaternion.Euler(0, 0, -degPerSec * Time.deltaTime);
+        print(GetSeason());
+    }
+
+    public string GetSeason()
+    {
+        var deg = transform.localEulerAngles.z;
+        if (360 >= deg && deg > 243) return "Spring";
+        if (243 >= deg && deg > 132) return "Summer";
+        if (132 >= deg && deg > 42) return "Fall";
+        return "Winter";
     }
 }
